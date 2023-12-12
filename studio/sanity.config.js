842e92248cc175c928659fcf9dfb7e83e8bed1c2
Chad <cmueller@rynoss.com>
2023-12-12 15:52:25 -0700

    add new theme style to config

--- studio/sanity.config.js
+++ studio/sanity.config.js
@@ -2,6 +2,7 @@
 import schemas from './schemas/schema'
 import deskStructure from './deskStructure'
 import Logo from "./plugins/ryno-studio-logo/Logo";
+import './styles/theme.css';
     deskTool({
         structure: deskStructure
     })
