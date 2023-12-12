77b5a2ace7992dca71d267e77dd92998f51b1388
Chad <cmueller@rynoss.com>
2023-12-12 15:50:10 -0700

    convert stuido logo and branding

--- studio/sanity.config.js
+++ studio/sanity.config.js
@@ -1,9 +1,15 @@
 // sanity.config.js
 import schemas from './schemas/schema'
 import deskStructure from './deskStructure'
+import Logo from "./plugins/ryno-studio-logo/Logo";
     deskTool({
         structure: deskStructure
     })
   schema: {
     types: schemas,
   },
+  studio: {
+    components: {
+        logo: Logo
+    }
+  }
