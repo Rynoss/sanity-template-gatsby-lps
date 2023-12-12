8b39eef354ddd7edf6336fed4d8703da216215ca
Chad <cmueller@rynoss.com>
2023-12-12 15:32:43 -0700

    add desk tool and schemas

---
+++ studio/sanity.config.js
@@ -0,0 +1,8 @@
+// sanity.config.js
+import schemas from './schemas/schema'
+import deskStructure from './deskStructure'
+    deskTool({
+    })
+  schema: {
+    types: schemas,
+  },
--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -1,9 +1,3 @@
-// First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator';
-
-// Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type';
-
 import categories from './categories';
 import companyInfo from './objects/companyInfo';
 import pages from './pages';
@@ -17,14 +11,7 @@ import location from './objects/location';
 import service from './objects/service';
 import colorOverides from './objects/colorOverrides';
 
-// Then we give our schema to the builder and provide the result to Sanity
-export default createSchema({
-  // We name our schema
-  name: 'default',
-  // Then proceed to concatenate our document type
-  // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([
-    /* Your types here! */
+export default [
     location,
     locations,
     categories,
@@ -37,5 +24,4 @@ export default createSchema({
     colorOverides,
     portableText,
     figure,
-  ]),
-});
+];
