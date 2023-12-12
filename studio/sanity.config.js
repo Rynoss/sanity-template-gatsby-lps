00ee2eeb6b66877a8aac35f04dfd81acebe064d8
Chad <cmueller@rynoss.com>
2023-12-12 15:53:01 -0700

    transition from json config to js config

--- studio/sanity.config.js
+++ studio/sanity.config.js
@@ -1,12 +1,29 @@
 // sanity.config.js
+import { defineConfig } from "sanity";
 import { deskTool } from 'sanity/desk'
 import schemas from './schemas/schema'
 import deskStructure from './deskStructure'
 import Logo from "./plugins/ryno-studio-logo/Logo";
 import './styles/theme.css';
+
+export default defineConfig({
+  title: "<#< sanity.projectTitle >#>",
+  projectId: "<#< sanity.projectId >#>",
+  dataset: "<#< sanity.dataset >#>",
+  plugins: [
+    //"@sanity/base",
+    //"@sanity/default-layout",
+    //"@sanity/default-login",
+    //"@sanity/desk-tool",
     deskTool({
         structure: deskStructure
     })
+    //"@sanity/color-input",
+    //"@sanity/dashboard",
+    //"dashboard-widget-netlify",
+    //"cloudinary",
+    // Add '@sanity/vision' plugin for development environment if needed
+  ],
   schema: {
     types: schemas,
   },
@@ -15,3 +32,4 @@ import './styles/theme.css';
         logo: Logo
     }
   }
+});
